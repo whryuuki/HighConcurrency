1591f858a114f5915493094c500154eda937c6d3
whryuuki <929699294@qq.com>
2019-08-05 09:29:40 +0800

    8-5

--- src/main/java/com/mmall/concurrency/annoations/NotThreadSafe.java
+++ src/main/java/com/mmall/concurrency/annoations/NotThreadSafe.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 课程里用来标记【线程不安全】的类或者写法
+ * 用来标记【线程不安全】的类或者写法
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.SOURCE)
